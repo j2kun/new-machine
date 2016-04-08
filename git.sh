f0ae247e9e82ca927f53f9dfbfb3fd5f321487d1
Jeremy Kun <kun.jeremy@gmail.com>
2016-04-07 22:23:55 -0700

    added global git settings

--- git.sh
+++ git.sh
@@ -2,3 +2,8 @@
 
 cp gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
+
+git config --global user.name "Jeremy Kun"
+git config --global user.email kun.jeremy@gmail.com
+git config --global core.editor vim
+git config --global github.user j2kun
