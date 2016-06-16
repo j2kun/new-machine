9c524394c6d7dc8df8b31dba4df9b398c706eeef
Jeremy Kun <jeremy@21.co>
2016-06-16 09:50:50 -0700

    added a simple override for default mac keybindings

---
+++ keybindings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Overrides default OS X keybindings with, e.g., math shortcuts
+mkdir -p ~/Library/KeyBindings
+cp osx-config-files/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
