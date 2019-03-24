e48cd849f089cc845d1f5347a534a51016fc16c6
Jeremy Kun <j2kun@users.noreply.github.com>
2019-03-24 19:06:37 +0100

    more brew, move cask

---
+++ install-via-brew-cask.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+brew install caskroom/cask/brew-cask
+
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
+# brew cask install google-chrome
+brew cask install transmission
+brew cask install dropbox
+brew cask install skype
+brew cask install vlc
+brew cask install skim
+brew cask install flux
+brew cask install spectacle
+brew cask install gimp
--- install-via-brew.sh
+++ install-via-brew.sh
@@ -2,28 +2,20 @@
 
 # command line tools
 brew install wget
-brew install lame 
-brew install flac 
+brew install lame
+brew install flac
 brew install ffmpeg
 brew install fasd
 brew install vim
 brew install jq
-brew install zsh 
-brew install rename
-brew install tree
+brew install zsh
+brew install tree      # view dir as tree
 brew install cmake
-
-# Applications
-brew install caskroom/cask/brew-cask
-# brew cask install google-chrome
-brew cask install transmission
-brew cask install dropbox
-brew cask install skype 
-brew cask install vlc
-brew cask install skim 
-brew cask install flux
-brew cask install spectacle 
-brew cask install gimp
+brew install ripgrep   # faster grep
+brew install xsv       # csv CLI toolkit
+brew install bat       # cat with wings
+brew install exa       # ls replacement
+brew install httpie
 
 # install python
 brew install python3
